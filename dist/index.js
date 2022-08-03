2c65581316dce137b7a422b10480b2d3f40f451d
Tomoatsu Sekikawa <gizumon@TomoatsunoAir.AirPort>
2022-08-03 22:54:20 +0900

    Test commit

--- dist/index.js
+++ dist/index.js
@@ -48,8 +48,9 @@ function run() {
             core.setOutput('time', new Date().toTimeString());
         }
         catch (error) {
-            if (error instanceof Error)
+            if (error instanceof Error) {
                 core.setFailed(error.message);
+            }
         }
     });
 }
