cd26b27e3ba8501ca7250a3d57bb4b3afc9bd08a
Tomoatsu Sekikawa <a.g0430t.s@gmail.com>
2022-08-07 22:52:16 +0900

    Fixed ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable prettier/prettier */
 import * as core from '@actions/core'
-import * as github from '@actions/github'
-import * as exec from '@actions/exec'
+// import * as github from '@actions/github'
+// import * as exec from '@actions/exec'
 
 import {
   mdCommonHeader,
@@ -54,72 +54,73 @@ const runMain = async (): Promise<void> => {
     log('Done generate markdown processes ...')
     log(result)
 
-    const token = process.env.GITHUB_TOKEN || ''
-    log('token: ' + token)
-    if (token && !props.generateOnly) {
-      log('Run result commit and push...')
-      exec.exec('echo', [
-        `'${result}'`,
-        props.overwrite ? '>' : '>>',
-        props.documentPath,
-      ])
-      exec.exec('echo', [
-        'test',
-        props.overwrite ? '>' : '>>',
-        '$(eval echo "test.txt")',
-      ])
-      exec.exec('ls', ['-l'])
-      exec.exec('git', ['config', 'user.name', 'GitHub Action Documentator'])
-      exec.exec('git', ['config', 'user.email', 'github-action.com'])
-      log(await exec.getExecOutput('git', ['status']))
-      exec.exec('git', ['add', props.documentPath])
-      exec.exec('git', ['commit', '-m', 'Update reusable workflows document'])
-      const octokit = github.getOctokit(token, {
-        baseUrl: props.githubBaseUrl
-          ? props.githubBaseUrl
-          : 'https://api.github.com',
-      })
-      const context = github.context
+    // const token = process.env.GITHUB_TOKEN || ''
+    // log('token: ' + token)
+    // TODO: Fix this, not working for now due to the generated file cannot find.
+    // if (token && !props.generateOnly) {
+    //   log('Run result commit and push...')
+    //   exec.exec('echo', [
+    //     `'${result}'`,
+    //     props.overwrite ? '>' : '>>',
+    //     props.documentPath,
+    //   ])
+    //   exec.exec('echo', [
+    //     'test',
+    //     props.overwrite ? '>' : '>>',
+    //     '$(eval echo "test.txt")',
+    //   ])
+    //   exec.exec('ls', ['-l'])
+    //   exec.exec('git', ['config', 'user.name', 'GitHub Action Documentator'])
+    //   exec.exec('git', ['config', 'user.email', 'github-action.com'])
+    //   log(await exec.getExecOutput('git', ['status']))
+    //   exec.exec('git', ['add', props.documentPath])
+    //   exec.exec('git', ['commit', '-m', 'Update reusable workflows document'])
+    //   const octokit = github.getOctokit(token, {
+    //     baseUrl: props.githubBaseUrl
+    //       ? props.githubBaseUrl
+    //       : 'https://api.github.com',
+    //   })
+    //   const context = github.context
 
-      // const defaultBranch = context.payload.repository?.default_branch || 'main'
-      const owner = context.repo.owner
-      const repo = context.repo.repo
-      const headBranch = context.ref.replace('refs/heads/', '')
-      // const baseBranch = context.payload.pull_request
-      //   ? context.payload.pull_request.base.ref
-      //   : defaultBranch
+    //   // const defaultBranch = context.payload.repository?.default_branch || 'main'
+    //   const owner = context.repo.owner
+    //   const repo = context.repo.repo
+    //   const headBranch = context.ref.replace('refs/heads/', '')
+    //   // const baseBranch = context.payload.pull_request
+    //   //   ? context.payload.pull_request.base.ref
+    //   //   : defaultBranch
 
-      if (props.shouldMakePullRequest) {
-        // if make-pull-request is true, create a pull request
-        try {
-          const prBranch = `feature/reusable-workflow-documentator-${Date.now()}`
-          exec.exec('git', ['push', 'origin', prBranch])
-          const pullRequest = await octokit.rest.pulls.create({
-            owner,
-            repo,
-            title: '📝 Reusable Workflows Document',
-            body: result,
-            head: prBranch,
-            base: headBranch, // pr from prBranch to baseBranch
-          })
-          if (pullRequest.status < 300) {
-            log('Success create pull request')
-          } else {
-            log('Fail create pull request: ' + JSON.stringify(pullRequest))
-            core.setFailed(new Error('Failed to create pull request'))
-          }
-        } catch (err) {
-          log('Fail create pull request: ' + JSON.stringify(err))
-          core.setFailed(
-            err instanceof Error ? err : new Error(JSON.stringify(err))
-          )
-        }
-      } else {
-        // If make-pull-request is false, then push the changes to the head branch.
-        exec.exec('git', ['push', 'origin', headBranch])
-        log('Success push commit to ' + headBranch)
-      }
-    }
+    //   if (props.shouldMakePullRequest) {
+    //     // if make-pull-request is true, create a pull request
+    //     try {
+    //       const prBranch = `feature/reusable-workflow-documentator-${Date.now()}`
+    //       exec.exec('git', ['push', 'origin', prBranch])
+    //       const pullRequest = await octokit.rest.pulls.create({
+    //         owner,
+    //         repo,
+    //         title: '📝 Reusable Workflows Document',
+    //         body: result,
+    //         head: prBranch,
+    //         base: headBranch, // pr from prBranch to baseBranch
+    //       })
+    //       if (pullRequest.status < 300) {
+    //         log('Success create pull request')
+    //       } else {
+    //         log('Fail create pull request: ' + JSON.stringify(pullRequest))
+    //         core.setFailed(new Error('Failed to create pull request'))
+    //       }
+    //     } catch (err) {
+    //       log('Fail create pull request: ' + JSON.stringify(err))
+    //       core.setFailed(
+    //         err instanceof Error ? err : new Error(JSON.stringify(err))
+    //       )
+    //     }
+    //   } else {
+    //     // If make-pull-request is false, then push the changes to the head branch.
+    //     exec.exec('git', ['push', 'origin', headBranch])
+    //     log('Success push commit to ' + headBranch)
+    //   }
+    // }
   } catch (err) {
     core.setFailed(
       err instanceof Error ? err.message : `Unknown error: ${String(err)}`
